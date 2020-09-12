5d78e4cca961f419b2d9a2188871c09e2158f734
Ritwick Joshi <ritwickjoshi@gmail.com>
2020-09-12 22:59:11 +0530

    Did the domino challenge and created a master template

--- domino.cpp
+++ domino.cpp
@@ -1,9 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define gc getchar_unlocked
 #define fo(i,n) for(i=0;i<n;i++)
 #define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define ll long long
 #define si(x)	scanf("%d",&x)
 #define sl(x)	scanf("%lld",&x)
 #define ss(s)	scanf("%s",s)
@@ -12,9 +10,22 @@ using namespace std;
 #define ps(s)	printf("%s\n",s)
 #define deb(x) cout << #x << "=" << x << endl
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define PI 3.1415926535897932384626
 
 void solve() {
-  int i, j, n, m;
+  int n, m, max_dominoes=0;
+  si(m);si(n);
+  if (m%2==0){
+	  max_dominoes+=((m/2)*n);
+  }
+  else if(n%2==0){
+	  max_dominoes+=((n/2)*m);
+  }
+  else if((m%2!=0) && (n%2!=0)){
+	  m--;n--;
+	  max_dominoes+=(((m/2)*n) + (m/2) + (n/2));
+	  }
+	pi(max_dominoes);
 }
 
 int main() {
@@ -22,7 +33,7 @@ int main() {
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while(t--) {
       solve();
     }
